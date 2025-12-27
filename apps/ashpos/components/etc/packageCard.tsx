84be0cac9da5b99d73e7de348697ddd394464845
Polymath1108 <ruka59171@gmail.com>
2025-12-27 12:47:37 +0800

    feat: added status to packagebyLabel query

--- apps/ashpos/components/etc/packageCard.tsx
+++ apps/ashpos/components/etc/packageCard.tsx
@@ -48,7 +48,7 @@ export default function PackageCard({ packageLabel, packageData, isLoading, hand
     const [modalOpen, setModalOpen] = useState(false);
     const [isUpdate, setIsUpdate] = useState(false);
 
-    // console.log('packageData', packageData);
+    console.log('packageData', packageData);
 
     // useEffect(() => {
     //     if (isUpdate) {
